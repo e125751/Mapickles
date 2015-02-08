a134c5abad6b427b123f8b469df938cdb6aa6b71
e125751 <e125751@ie.u-ryukyu.ac.jp>
2015-02-09 01:03:11 +0900

    add question1

---
+++ Mapickles/Question1.swift
@@ -0,0 +1,32 @@
+//
+//  question1.swift
+//  unwindToTop
+//
+//  Created by motoshi on 2015/02/08.
+//  Copyright (c) 2015年 motoshi. All rights reserved.
+//
+
+import UIKit
+
+class Question1: UIViewController {
+    
+    var appDelegate:AppDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func selectedFoot(sender: AnyObject) {
+        appDelegate.answer1 = "foot"
+    }
+    
+    @IBAction func selectedCar(sender: AnyObject) {
+        appDelegate.answer1 = "car"
+    }
+}
