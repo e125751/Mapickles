c20e1a1204146b5168bb4becb99aad790bdef5fd
e125751 <e125751@ie.u-ryukyu.ac.jp>
2015-02-09 19:38:32 +0900

    add ResultTableView

---
+++ ResultTableController.swift
@@ -0,0 +1,23 @@
+//
+//  ResultTableController.swift
+//  Mapickles
+//
+//  Created by motoshi on 2015/02/09.
+//  Copyright (c) 2015年 motoshi. All rights reserved.
+//
+
+import UIKit
+
+class ResultTableController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+}
