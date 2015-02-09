7c818f2f78f6a41cbde73928f4448e1d7ca75431
e125751 <e125751@ie.u-ryukyu.ac.jp>
2015-02-10 01:26:54 +0900

    add Table contents

---
+++ Mapickles/ResultTableController.swift
@@ -0,0 +1,60 @@
+//
+//  ResultTableController.swift
+//  Mapickles
+//
+//  Created by motoshi on 2015/02/10.
+//  Copyright (c) 2015年 motoshi. All rights reserved.
+//
+
+import UIKit
+
+
+class ResultTableController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    // Tableで使用する配列を設定する
+    let myItems: NSArray = ["TEST1", "TEST2", "TEST3"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let barHeight: CGFloat = UIApplication.sharedApplication().statusBarFrame.size.height
+        
+        let displayWidth: CGFloat = self.view.frame.width
+        let displayHeight: CGFloat = self.view.frame.height
+        
+        let myTableView: UITableView = UITableView(frame: CGRect(x: 0, y: barHeight, width: displayWidth, height: displayHeight - barHeight))
+        
+        myTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+        
+        myTableView.dataSource = self
+        
+        myTableView.delegate = self
+        
+        self.view.addSubview(myTableView)
+        
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        println("Num: \(indexPath.row)")
+        println("Value: \(myItems[indexPath.row])")
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myItems.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("MyCell", forIndexPath: indexPath) as UITableViewCell
+        
+        cell.textLabel!.text = "\(myItems[indexPath.row])"
+        
+        return cell
+    }
+    
+}
