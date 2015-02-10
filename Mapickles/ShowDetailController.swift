6255293dbe6c7f8785e6bf1288da8dd07261fc27
e125751 <e125751@ie.u-ryukyu.ac.jp>
2015-02-10 09:42:30 +0900

    add UILabel to Detail

--- Mapickles/ShowDetailController.swift
+++ Mapickles/ShowDetailController.swift
@@ -13,6 +13,29 @@ class ShowDetailController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // Labelを作成.
+        
+        let myLabel: UILabel = UILabel(frame: CGRectMake(0,0,200,50))
+        
+        myLabel.backgroundColor = UIColor.orangeColor()
+        
+        myLabel.layer.masksToBounds = true
+        
+        myLabel.layer.cornerRadius = 20.0
+        
+        myLabel.text = "Hello Swift!!"
+        
+        myLabel.textColor = UIColor.whiteColor()
+        
+        myLabel.shadowColor = UIColor.grayColor()
+        
+        myLabel.textAlignment = NSTextAlignment.Center
+        
+        myLabel.layer.position = CGPoint(x: self.view.bounds.width/2,y: 200)
+        
+        self.view.backgroundColor = UIColor.cyanColor()
+        
+        self.view.addSubview(myLabel)
     }
     
     override func didReceiveMemoryWarning() {
